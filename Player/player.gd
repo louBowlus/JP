a933e2958e9b10fd9be5cdb9e1190a2103babd8c
louBowlus <loubowlus@gmail.com>
2023-01-27 21:25:57 -0800

    player sprites

--- Player/player.gd
+++ Player/player.gd
@@ -1,45 +1,61 @@
 extends KinematicBody2D
 
-
-
 #movement here
-const speed = 10000
-const maxSpeed = 20000
-const friction = 7000
+const speed = 5000
+const maxSpeed = 10000
+const friction = 3500
 
 var velocity = Vector2.ZERO
 
+#animation nodes
 onready var animationPlayer = $AnimationPlayer
+onready var dashPlayer = $DashPlayer
 
+#state tracking
 enum {
 	MOVE,
 	DASH,
-	
 }
 
 var state = MOVE
 var dashVector = Vector2.ZERO
 
+#sprite fading
 onready var fadeContainer = $FadeContainer
-var playerSprite = preload("res://icon.png")
-var fades = []
+var playerSprite = preload("res://Player/base.png")
+onready var sprite = $Icon
 
 func _physics_process(delta):
+	#get input
 	var a = Vector2.ZERO
 	
 	a.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	a.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")\
+	a.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	a = a.normalized()
+	
+	#flip sprite
+	if a.x < 0:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
 	
 	match state:
 		MOVE:
+			#dash
 			if Input.is_action_just_pressed("dash") and a != Vector2.ZERO:
-				animationPlayer.play("dash")
+				dashPlayer.play("dash")
 				dashVector = a
 				state = DASH
 				
 			
-			a = a.normalized()
+			#animate
+			if a == Vector2.ZERO:
+				animationPlayer.play("idle")
+			else:
+				animationPlayer.play("run")
 			
+			
+			#acceleration and friction
 			velocity += a * speed * delta
 			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 			velocity.x = clamp(velocity.x, -maxSpeed * delta, maxSpeed * delta)
@@ -48,16 +64,18 @@ func _physics_process(delta):
 			velocity = move_and_slide(velocity)
 			
 		DASH:
+			#make all of the afterimages fade
 			for child in fadeContainer.get_children():
 				child.modulate.a -= 0.20
 				if child.modulate.a <= 0:
 					child.queue_free()
-				print(child.modulate)
 			
+			#player can manipulate the dash direction a little bit
 			velocity = dashVector * maxSpeed * delta * 2
-			velocity += a * 100
+			velocity += a * 200
 			velocity = move_and_slide(velocity)
 		
+		#if all fades gone, then stop the dash
 			if fadeContainer.get_child_count() == 0:
 				state = MOVE
 				dashVector = Vector2.ZERO
@@ -65,8 +83,11 @@ func _physics_process(delta):
 
 
 func addFade():
+	#make a sprite a child of a Node to stay in place without following the player
 	var s = Sprite.new()
 	s.texture = playerSprite
+	s.scale *= self.scale
 	s.global_position = global_position
 	fadeContainer.add_child(s)
+	s.flip_h = sprite.flip_h
 	
